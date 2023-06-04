66d2a567be84fb2df56c11b53091dae6991b7c5e
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2023-06-04 23:57:13 +0530

    Working Initial Commit

--- staybnb-booking-app/app/hooks/useFavorite.ts
+++ staybnb-booking-app/app/hooks/useFavorite.ts
@@ -1,7 +1,9 @@
 import { useRouter } from "next/navigation";
 import { SafeUser } from "../types";
 import useLoginModal from "./useLoginModal";
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 
 interface UseFavoriteProps {
@@ -22,6 +24,34 @@ const useFavorite = ({
         return list.includes(listingId);
     },[currentUser, listingId]);
 
-    
+    const toggleFavorite = useCallback(async(e: React.MouseEvent<HTMLDivElement>)=>{
+        e.stopPropagation();
+
+        if(!currentUser){
+            return loginModal.onOpen();
+        }
+        try{
+            let request;
+
+            if(hasFavorited){
+                request = () => axios.delete(`/api/favorites/${listingId}`);
+            } else [
+                request = () => axios.post(`/api/favorites/${listingId}`)
+            ]
+
+            await request();
+            router.refresh();
+            toast.success('Success')
+        } catch (error){
+            toast.error('Something went wrong.')
+        }
+    },[currentUser, hasFavorited, listingId, loginModal, router])
+
+    return {
+        hasFavorited,
+        toggleFavorite
+    }
 
 }
+
+export default useFavorite;
