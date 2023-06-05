81dc2d07252b38bb72a98d155f7dea0bba1c18ac
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2023-06-05 12:38:17 +0530

    Working Status Commit

--- staybnb-booking-app/app/listings/[listingId]/page.tsx
+++ staybnb-booking-app/app/listings/[listingId]/page.tsx
@@ -1,7 +1,23 @@
+import getListingById from "@/app/actions/getListingById";
+import EmptyState from "@/app/components/EmptyState";
 
+interface IParams {
+  listingId? : string;
+}
 
-const ListingPage = () => {
-  return <div>page</div>;
+const ListingPage = async({ params } : { params: IParams }) => {
+  const listing = await getListingById(params); 
+
+  if(!listing){
+    return(
+      <EmptyState/>
+    )
+  }
+
+  return (
+    <div>
+      {listing.title}
+    </div>);
 };
 
 export default ListingPage;
