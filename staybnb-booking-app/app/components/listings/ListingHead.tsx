a672f0b195bc204a9bab9b8b4cf72bbfbfc9241e
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2023-06-06 11:42:06 +0530

    Working Commit

--- staybnb-booking-app/app/components/listings/ListingHead.tsx
+++ staybnb-booking-app/app/components/listings/ListingHead.tsx
@@ -4,6 +4,7 @@ import { SafeUser } from "@/app/types";
 import Heading from "../Heading";
 import useCountries from "@/app/hooks/useCountries";
 import Image from "next/image";
+import HeartButton from "../HeartButton";
 
 interface ListingHeadProps {
     title : string;
@@ -35,6 +36,12 @@ const ListingHead: React.FC<ListingHeadProps> = ({
                 fill
                 className="object-cover w-full"
             />
+            <div className="absolute top-5 right-5">
+                <HeartButton
+                    listingId={id}
+                    currentUser={currentUser}
+                />
+            </div>
         </div>
     </>
   )
