33d2243d73ec394ca448d5cfb2fc65ac994509af
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2023-06-07 23:25:23 +0530

    Working Commit

---
+++ staybnb-booking-app/app/reservations/ReservationsClient.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import Container from "../components/Container"
+import Heading from "../components/Heading"
+
+const ReservationsClient = () => {
+  return (
+    <Container>
+        <Heading
+            title="Reservations"
+            subtitle="Bookings on your properties"
+        />
+    </Container>
+  )
+}
+
+export default ReservationsClient
