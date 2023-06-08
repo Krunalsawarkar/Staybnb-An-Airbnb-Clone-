44050e8cfd1463be565ed4e47a382b030bb114c6
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2023-06-08 21:28:33 +0530

    Finalized Project

--- staybnb-booking-app/app/layout.tsx
+++ staybnb-booking-app/app/layout.tsx
@@ -31,8 +31,10 @@ export default async function RootLayout({
         <LoginModal />
         <RegisterModal />
         <Navbar currentUser={currentUser} />
-        <div className="pb-20 pt-28
-        ">
+        <div
+          className="pb-20 pt-28
+        "
+        >
           {children}
         </div>
       </body>
